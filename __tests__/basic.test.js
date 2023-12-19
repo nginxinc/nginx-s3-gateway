84febfea617e5328ebb3d8eb5ca0f21b76d0d2c2
Javier Evans <j.evans@f5.com>
2023-12-18 18:39:28 -0800

    basic integration test working

--- __tests__/basic.test.js
+++ __tests__/basic.test.js
@@ -1,22 +1,20 @@
 
 const Minio = require("minio");
+const request = require("supertest");
 
-const FILES = [
-  {
-    path: "/foo/bar.txt",
-    content: "dog"
+const FILES = {
+  "/foo/bar.txt": {
+    content: "dog",
   },
-  {
-    path: "/foo/baz.txt",
-    content: "cat"
+  "/foo/baz.txt": {
+    content: "cat",
   },
-  {
-    path: "/grumpy.txt",
-    content: "bah"
-  }
-];
+  "/grumpy.txt": {
+    content: "bah",
+  },
+};
 
-const BUCKET_NAME = "javier";
+const BUCKET_NAME = "bucket-1";
 
 beforeAll(async () => {
   const minioClient = new Minio.Client({
@@ -27,25 +25,30 @@ beforeAll(async () => {
     secretKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
   });
 
-  ensureBucketWithObjects(minioClient, BUCKET_NAME, FILES);
+  await ensureBucketWithObjects(minioClient, BUCKET_NAME, FILES);
 });
 
 async function ensureBucketWithObjects(s3Client, bucketName, objects) {
   if (await s3Client.bucketExists(BUCKET_NAME)) {
-    await s3Client.removeObjects(BUCKET_NAME, FILES.map((f) => f.path));
+    await s3Client.removeObjects(BUCKET_NAME, Object.keys(FILES));
     await s3Client.removeBucket(BUCKET_NAME);
   }
 
   await s3Client.makeBucket(BUCKET_NAME, 'us-east-1');
 
-  for (const i in FILES) {
-    console.log(`now loading file ${FILES[i]}`)
-    let buf = Buffer.from(FILES[i].content, "utf-8");
-    let res = await s3Client.putObject(BUCKET_NAME, FILES[i].path, buf);
+  for (const path of Object.keys(FILES)) {
+    console.log(`now loading file ${path}`);
+    let buf = Buffer.from(FILES[path].content, "utf-8");
+    let res = await s3Client.putObject(BUCKET_NAME, path, buf);
     console.log(`Uploaded file: ${JSON.stringify(res)}`);
   }
 }
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(true).toBe(true);
+test('adds 1 + 2 to equal 3', async () => {
+  const res = await request('http://localhost:8989')
+    .get("/foo/bar.txt")
+    .set("accept", "binary/octet-stream");
+
+  expect(res.statusCode).toBe(200);
+  expect(res.text).toBe(FILES["/foo/bar.txt"].content);
 });
